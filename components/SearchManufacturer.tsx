b8f699bd11df7e4b0196a1e44b4239a5d3686aed
Proprogam <k.kartal9595@gmail.com>
2023-07-05 20:24:30 +0300

    custom filter combobox resolved

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -54,7 +54,7 @@ const SearchManufacturer = ({
                   value={query}
                   className="search-manufacturer__option"
                 >
-                  Create "{query}"
+                  Create {query}
                 </Combobox.Option>
               ) : (
                 filteredManufacturers.map((item) => (
